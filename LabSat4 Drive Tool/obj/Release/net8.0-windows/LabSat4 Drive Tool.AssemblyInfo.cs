784bc9491f69d2628b41406843c881dab363f5ef
Steve Griff <ssg-sas@outlook.com>
2024-04-15 13:35:51 +0100

    includes installer

--- LabSat4 Drive Tool/obj/Release/net8.0-windows/LabSat4 Drive Tool.AssemblyInfo.cs
+++ LabSat4 Drive Tool/obj/Release/net8.0-windows/LabSat4 Drive Tool.AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabSat4 Drive Tool")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df353d2993759e58cd32155e64dce0a121973be2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabSat4 Drive Tool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabSat4 Drive Tool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
